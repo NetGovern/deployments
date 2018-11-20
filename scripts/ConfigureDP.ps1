188eb478b953aaad265838a905a012171f3857a9
Santiago Vazquez <santiagov@netmail.com>
2018-11-20 17:41:50 -0500

    Fix encrypted password regex issue

--- scripts/ConfigureDP.ps1
+++ scripts/ConfigureDP.ps1
@@ -36,16 +36,14 @@ $rp_credentials = new-object -typename System.Management.Automation.PSCredential
 New-PSDrive -name "DP" -PSProvider FileSystem -Root "\\$rp_server_address\c$\Program Files (x86)\Messaging Architects" -Credential $rp_credentials
 
 #Find next available port
-$ssl_ports_array = @()
-$http_ports_array = @()
+$ssl_ports_array = @(8443)
+$http_ports_array = @(8888)
 Get-ChildItem -Path  "DP:\Netmail Webadmin\etc\launcher.d" -Filter 60-awa*.conf |
 Foreach-Object {
-    $ssl_port = 0
-    $http_port = 0
     $a = Select-String -Path $_.FullName -Pattern "start" # | Out-String
     $splitString = [regex]::Split( $a.Line, ' (?=(?:[^"]|"[^"]*")*$)' )
-
     $remote_path = Split-Path ($splitString[3] -replace '"','')
+    $remote_path = $remote_path.Replace('C:\Program Files (x86)\Messaging Architects', 'DP:')
     $ssl_port = [xml]((Select-String -Path "$remote_path\jetty-ssl.xml" -Pattern 'Set name="port"').Line.Trim())
     $http_port = (Select-String -Path "$remote_path\xgwxmlv.cfg" -Pattern 'provider.http.port').Line.Trim()
     if ($http_port -notmatch "^\#") {
@@ -94,10 +92,6 @@ $jetty_ssl -replace 'port".+', "port`">$ssl_free_port</Set>" | Out-File -FilePat
 Write-Host "Configure xgwxmlv.cfg"
 #Getting data from master config files
 $edir_properties_nipe_master = "$env:NETMAIL_BASE_DIR\..\Nipe\Config\edir.properties"
-$cluster_config_conn_string = (Select-String 'PRMS' "$env:NETMAIL_BASE_DIR\..\Config\ClusterConfig.xml").Line
-[regex]$loginpwdclear_regex='PASSWORD=".*?"'
-$loginpwdclear = ($loginpwdclear_regex.Matches($cluster_config_conn_string) |`
-    ForEach-Object {$_.Value}).replace('"', '').split('=')[1]
 
 $ipaddress = (Test-Connection -ComputerName $env:COMPUTERNAME -Count 1 | Select-Object IPV4Address).IPV4Address
 
@@ -123,6 +117,10 @@ $xgwxmlv_cfg = $xgwxmlv_cfg -replace "edir.port=.+", `
 $xgwxmlv_cfg = $xgwxmlv_cfg -replace "edir.container=.+", `
     "edir.container=$(((Select-String 'edir.container=' $edir_properties_nipe_master).Line -split '=',2)[1])"
 
+$eclients_password = "$(((Select-String 'edir.loginpwdclear=' $edir_properties_nipe_master).Line -split '=',2)[1])"
+$eclients_password_encrypted = ( & $env:NETMAIL_BASE_DIR\etc\scripts\setup\Win-edir\InstallUtils.exe mode=enc value="$eclients_password").trim()
+$xgwxmlv_cfg = $xgwxmlv_cfg -replace "edir.loginpwd=.+", "edir.loginpwd=$eclients_password_encrypted"
+
 $xgwxmlv_cfg | Out-File -FilePath "DP:\RemoteProvider_$tenant_id\xgwxmlv.cfg"
 
 Write-Host "Create launcher config file"
@@ -132,7 +130,6 @@ Write-Host "Create launcher config file"
     Out-File -Append -FilePath "DP:\Netmail WebAdmin\etc\launcher.d\60-awa_$tenant_id.conf"
 Remove-PSDrive -Name DP
 
-Write-Output $http_free_port
 
 
 
