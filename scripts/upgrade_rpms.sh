13b5d7e797cafae3f1bd74e778137e137104fb0f
Santiago Vazquez <santiagov@netmail.com>
2019-02-04 20:46:38 -0500

    Add option to install platform RPM only

--- scripts/upgrade_rpms.sh
+++ scripts/upgrade_rpms.sh
@@ -4,22 +4,24 @@
 # Required arguments
 usage() { 
     echo "IMPORTANT! Enclose the password in single quotes to make sure that special characters are escaped.
-    Usage: $0 -v '<netmail version>'
+    Usage: $0 -v '<netgovern version> [-p][-i][-s]'
     [-i]    Installs index RPM
     [-s]    Installs secure RPM
+    [-p]    Installs platform RPM only
     " 1>&2; exit 1; 
 }
 
 # Default Values
 SECURE=0
 INDEX=0
+PLATFORM=0
 INDEX_RPM=""
 SECURE_RPM=""
 
-while getopts ":v:si" o; do
+while getopts ":v:sip" o; do
     case "${o}" in
         v)
-            NETMAIL_VERSION=${OPTARG}
+            NETGOVERN_VERSION=${OPTARG}
             ;;
         s)
             SECURE=1
@@ -27,6 +29,9 @@ while getopts ":v:si" o; do
         i)
             INDEX=1
             ;;
+        p)
+            PLATFORM=1
+            ;;  
         *)
             usage
             ;;
@@ -34,24 +39,32 @@ while getopts ":v:si" o; do
 done
 shift $((OPTIND-1))
 
-if [ -z "${NETMAIL_VERSION}" ]; then
+if [ -z "${NETGOVERN_VERSION}" ]; then
     usage
     exit 1
 fi
 
+if [[ ${NETGOVERN_VERSION} < "9.9.9.9" ]]; then # If (or when) RPMs change their name in future versions.  
+                                                # Replace 9.9.9.9 with the version bringing the name change
+    BRAND="NETMAIL"
+else
+    BRAND="NETGOVERN"
+fi
+
+
 # Download and install RPMs
-PLATFORM_RPM="CENTOS-NETMAIL-PLATFORM-${NETMAIL_VERSION}_release-Linux.rpm"
+PLATFORM_RPM="CENTOS-${BRAND}-PLATFORM-${NETGOVERN_VERSION}_release-Linux.rpm"
 wget --no-check-certificate \
     https://netgovernpkgs.blob.core.windows.net/download/${PLATFORM_RPM}
 
 if [ ${INDEX} -eq 1 ]; then
-    INDEX_RPM="CENTOS-NETMAIL-INDEX-${NETMAIL_VERSION}_release-Linux.rpm"
+    INDEX_RPM="CENTOS-${BRAND}-INDEX-${NETGOVERN_VERSION}_release-Linux.rpm"
     wget --no-check-certificate \
         https://netgovernpkgs.blob.core.windows.net/download/${INDEX_RPM}
 fi
 
 if [ ${SECURE} -eq 1 ]; then
-    SECURE_RPM="CENTOS-NETMAIL-SECURE-${NETMAIL_VERSION}_release-Linux.rpm"
+    SECURE_RPM="CENTOS-${BRAND}-SECURE-${NETGOVERN_VERSION}_release-Linux.rpm"
     wget --no-check-certificate \
         https://netgovernpkgs.blob.core.windows.net/download/${SECURE_RPM}
 fi
