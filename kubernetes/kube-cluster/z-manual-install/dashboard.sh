22182929f163d1d4554a7d37be095a7eedbef61e
Santiago <24659153+svzez@users.noreply.github.com>
2020-05-20 08:56:33 -0400

    Update link to yaml

--- kubernetes/kube-cluster/z-manual-install/dashboard.sh
+++ kubernetes/kube-cluster/z-manual-install/dashboard.sh
@@ -6,9 +6,9 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/
 echo "Patching k8s dashboard service to NodePort"
 kubectl -n kube-system patch svc kubernetes-dashboard -p '{"spec":{"type":"NodePort"}}'
 
-kubectl apply -f https://bitbucket.netmail.com/projects/PUB/repos/deployments/raw/kubernetes/kube-cluster/a-%20dashboard-adminuser.yaml?at=refs%2Fheads%2Fmaster
-kubectl apply -f https://bitbucket.netmail.com/projects/PUB/repos/deployments/raw/kubernetes/kube-cluster/b-%20rbac-auth.yaml?at=refs%2Fheads%2Fmaster
-kubectl apply -f https://bitbucket.netmail.com/projects/PUB/repos/deployments/raw/kubernetes/kube-cluster/c-%20rbac-config.yaml?at=refs%2Fheads%2Fmaster
+kubectl apply -f https://raw.githubusercontent.com/NetGovern/deployments/master/kubernetes/kube-cluster/a-%20dashboard-adminuser.yaml
+kubectl apply -f https://raw.githubusercontent.com/NetGovern/deployments/master/kubernetes/kube-cluster/b-%20rbac-auth.yaml
+kubectl apply -f https://raw.githubusercontent.com/NetGovern/deployments/master/kubernetes/kube-cluster/c-%20rbac-config.yaml
 
 echo "Get Dashboard port and Secret"
 echo "Port: $(kubectl -n kube-system get services kubernetes-dashboard | awk '{print $5}')"
